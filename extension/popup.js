0609d5f8b1b79c0eb062dfc05348973494603669
Abhishek Ranjan <abhishekrotary2002@gmail.com>
2021-03-21 07:11:59 +0530

    handle xhr warning and made model compatible with python3

--- extension/popup.js
+++ extension/popup.js
@@ -1,30 +1,33 @@
 function transfer() {
   var tablink;
   chrome.tabs.getSelected(null, function (tab) {
+    const original_url = tab.url
     tablink = tab.url;
     if (tablink.length > 30) {
-      tablink = tablink.slice(0, 30) + " ...";
+      tablink = tablink.slice(0, 30) + ' ...';
     }
-    $("#site").text(tablink);
+    $('#site').text(tablink);
 
     var xhr = new XMLHttpRequest();
-    params = "url=" + tablink;
+    params = 'url=' + original_url;
     var markup =
-      "url=" + tablink + "&html=" + document.documentElement.innerHTML;
-    xhr.open("POST", "http://localhost:8000", false);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      'url=' + original_url + '&html=' + document.documentElement.innerHTML;
+    xhr.open('POST', 'http://localhost:8000', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.onload = () => {
+      if (xhr.responseText === 'SAFE') {
+        $('#div1').text(xhr.responseText);
+      } else {
+        $('#div2').text(xhr.responseText);
+      }
+      return xhr.responseText;
+    };
     xhr.send(markup);
-    if (xhr.responseText === "SAFE") {
-      $("#div1").text(xhr.responseText);
-    } else {
-      $("#div2").text(xhr.responseText);
-    }
-    return xhr.responseText;
   });
 }
 
 $(document).ready(function () {
-  $("button").click(function () {
+  $('button').click(function () {
     var val = transfer();
   });
 });
@@ -32,7 +35,7 @@ $(document).ready(function () {
 chrome.tabs.getSelected(null, function (tab) {
   var tablink = tab.url;
   if (tablink.length > 30) {
-    tablink = tablink.slice(0, 30) + " ....";
+    tablink = tablink.slice(0, 30) + ' ....';
   }
-  $("#site").text(tablink + "\n\n");
+  $('#site').text(tablink + '\n\n');
 });
--- test/index.php
+++ test/index.php
@@ -1,8 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 $site=$_POST['url'];
-// $site="https://www.york.ac.uk/teaching/cws/wws/webpage1.html";
-// $site="http://phishtank.org/";
 $html = file_get_contents($site);
 $bytes=file_put_contents('markup.txt', $html);
 $decision=exec("python test.py $site 2>&1 ");
